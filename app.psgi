2f6962d5704ad371a09d91d436e17fad815c357a
Gryphon Shafer <gryphon@goldenguru.com>
2019-11-06 11:04:14 -0800

    Test checkpoint

--- app.psgi
+++ app.psgi
@@ -1,5 +1,6 @@
 #!/usr/bin/env perl
 
+# TODO: remove lib block
 use lib qw(
     /home/gryphon/cpan/exact/lib
     /home/gryphon/cpan/exact-class/lib
--- test.pl
+++ test.pl
@@ -1,10 +1,13 @@
 #!/usr/bin/env perl
+# TODO: delete this file
 
 use lib qw(
     /home/gryphon/cpan/exact/lib
     /home/gryphon/cpan/exact-class/lib
     /home/gryphon/pnwquizzing/lib
 );
+
+#-------------------------------------------------------------------------------
 
 # use PnwQuizzing::Model::Email;
 
@@ -19,7 +22,23 @@ use lib qw(
 #     },
 # });
 
+#-------------------------------------------------------------------------------
+
 # use PnwQuizzing::Model::Register;
 # my $meet = PnwQuizzing::Model::Register->new->next_meet;
 
+#-------------------------------------------------------------------------------
+
 use PnwQuizzing::Model::User;
+
+my $user = PnwQuizzing::Model::User->new;
+my $name = time;
+
+$user->create({
+    username   => $name,
+    passwd     => $name,
+    first_name => $name,
+    last_name  => $name,
+    email      => $name . '@example.com',
+    church     => 'PNWTI',
+});
