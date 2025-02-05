35ff510c04daf24f06c3de495480dc3d14e86ff9
Gryphon Shafer <gryphon@goldenguru.com>
2025-02-05 12:12:00 -0800

    Upgrade to Omniframe-provided Vue

--- static/js/user_list.js
+++ static/js/user_list.js
@@ -1,11 +1,14 @@
-var vm = new Vue({
-    el       : "#user_list",
-    data     : list_data,
-    computed : {
-        filtered_users : function () {
-            var selected_roles = this.roles.filter( (role) => role.selected ).map( (role) => role.name );
-            if ( selected_roles.length == 0 ) return this.users;
-            return this.users.filter( (user) => user.roles.some( (role) => selected_roles.includes(role) ) );
-        }
-    }
-});
+Vue
+    .createApp({
+        data() {
+            return list_data;
+        },
+        computed: {
+            filtered_users() {
+                const selected_roles = this.roles.filter( role => role.selected ).map( role => role.name );
+                if ( selected_roles.length == 0 ) return this.users;
+                return this.users.filter( user => user.roles.some( role => selected_roles.includes(role) ) );
+            },
+        },
+    })
+    .mount('#user_list');
